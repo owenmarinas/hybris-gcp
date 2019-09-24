d2f0586099874d4f9f86a1af53ee0b3e2756e46e
owen marinas <owenmarinas@gmail.com>
2019-09-24 03:48:02 +0000

    new machine-type w/ 4 CPU

--- common/build/06_gce-cluster.sh
+++ common/build/06_gce-cluster.sh
@@ -9,7 +9,7 @@ source ../project_settings.sh
 # Create a 3 node GCE cluster
 gcloud beta container clusters create $GCE_CLUSTER  \
   --project $PROJECT_NAME  --zone $PROJECT_ZONE  --no-enable-basic-auth \
-  --cluster-version "1.12.8-gke.10" --machine-type "n1-standard-1" --image-type "COS" --disk-type "pd-standard" --disk-size "100" \
+  --cluster-version "1.12.8-gke.10" --machine-type "n1-standard-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" \
   --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
   --num-nodes "4" --enable-stackdriver-kubernetes --enable-ip-alias --issue-client-certificate \
   --network $SERVICES_NETWORK  --subnetwork $SERVICES_SUBNET --default-max-pods-per-node "110"  \
